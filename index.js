2addd877e06d59869f458d5e2b4c577af23ca986
Akshay-Daundkar-14 <akshaydaundkar2000@gmail.com>
2023-09-05 10:30:48 +0530

    Notes 2 : Ignite app

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
- const element =  React.createElement('h1',{id:'heading'},'Hello world from REACT - H1');
-  const element2 =  React.createElement('h2',{id:'heading2'},'Hello world from REACT  - H2');
+import React from "react";
+//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+
+
+ 
+ const element =  React.createElement('h1',{id:'heading',key:"ele1"},'Hello world from REACT - H1');
+  const element2 =  React.createElement('h2',{id:'heading2',key:"ele2"},'Hello world from REACT  - H2');
 
 const child =  React.createElement('div',{id:'child'},[element,element2]);
 // const parent = React.createElement('div',{id:'parent'},child);
@@ -9,14 +15,10 @@ const child =  React.createElement('div',{id:'child'},[element,element2]);
 
 
 const parent = React.createElement('div',{id:'parent'},
-
 [
-
-    React.createElement('div',{id:'parent'},child),
-    React.createElement('div',{id:'parent'},child)
-
+    React.createElement('div',{id:'child1',key:"child1"},child),
+    React.createElement('div',{id:'child2',key:"child2"},child)
 ]
-
 );
 
 
@@ -27,14 +29,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
 
 //---------------------
-setTimeout(() => {
-    const top1 = ReactDOM.createRoot(document.getElementById('top'));
-top1.render(React.createElement('h1',{id:'myTop'},"TOP from react"));
-}, 5000);
+// setTimeout(() => {
+//     const top1 = ReactDOM.createRoot(document.getElementById('top'));
+// top1.render(React.createElement('h1',{id:'myTop'},"TOP from react"));
+// }, 5000);
 
 
-//---------------------
-setTimeout(() => {
-    const bottom1 = ReactDOM.createRoot(document.getElementById('bottom'));
-    bottom1.render(React.createElement('h1',{id:'myBottom'},"BOTTOM from react"));
-}, 5000);
+// //---------------------
+// setTimeout(() => {
+//     const bottom1 = ReactDOM.createRoot(document.getElementById('bottom'));
+//     bottom1.render(React.createElement('h1',{id:'myBottom'},"BOTTOM from react"));
+// }, 5000);
