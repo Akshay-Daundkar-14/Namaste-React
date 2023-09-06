fc49fdc579bfbd7fe6424c307191d02f784b0bf2
Akshay-Daundkar-14 <akshaydaundkar2000@gmail.com>
2023-09-07 00:28:37 +0530

    Filter Top Restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,41 +9,35 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    console.log("fetch data called");
-
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
       {
         method: "GET",
       }
     );
-
     const jsonData = await data.json();
-
-    console.log(
-      "DATA ",
-      jsonData.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
-    );
-    console.log(
-      "DATA ",
-      jsonData.data.cards[5].card.card.gridElements.infoWithStyle.restaurants[0]
-        .info
-    );
     setListOfRestaurants(
       jsonData.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
     );
   };
 
+  const filterTopRestaurant = ()=>{
+    const filteredData = listOfRestaurants.filter((res)=>res.info.avgRating >= 4);
+    setListOfRestaurants(filteredData);
+  }
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="search">
+        <button onClick={filterTopRestaurant}>Top Restaurants</button>
+      </div>
       <div className="res-container">
         {/* {listOfRestaurants.map((res)=>
           <RestraurantCard resName={res.info.name} key={res.info.id} imageId={res.info.cloudinaryImageId}
           cuisine={res.info.cuisines.join(',')} rating={res.info.avgRating} minutes={res.info.sla.slaString}/>
         )} */}
          {listOfRestaurants.map((res)=>
-          <RestraurantCard resData={res.info}/>
+          <RestraurantCard resData={res.info} key={res.info.id}/>
         )}
       </div>
     </div>
